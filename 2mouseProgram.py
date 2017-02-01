3219280541ab36218f01340b911dbd12624e50e4
ngardner17 <ngardner17@student.vcschools.org>
2017-02-01 01:51:14 +0000

    Changed program to work from the top of the loop when roborio disconnects

--- 2mouseProgram.py
+++ 2mouseProgram.py
@@ -16,22 +16,24 @@ yf1 = 0.0
 host = '10.44.15.35'
 port = 5800
 
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print("Socket created")
+time.sleep(.1)
+server_socket.bind((host, port))
+
 while 1:
 	serverConnected = False
 	if serverConnected == False:
-		server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		print ("Socket created")
-		time.sleep(.1)
-		server_socket.bind((host, port))
+		#server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		#print ("Socket created")
+		#time.sleep(.1)
+		#server_socket.bind((host, port))
 		server_socket.listen(1)
 		print ("Listening in socket")
 		conn, addr = server_socket.accept()
 		print ("Connected with " + addr[0] + ":" + str(addr[1]))
 		serverConnected = True
 		conn.settimeout(1)
-		#alpha = 90 - theta - math.degrees(math.atan((xm1 - xm0) / (ym1 - ym0)))
-		#print ((math.sqrt((xm1-xm0) * (ym1 - ym0)) * math.cos(alpha))
-		#try:
 	while serverConnected == True:
 		xm0 = xm1
 		ym0 = ym1
@@ -44,14 +46,9 @@ while 1:
 			serverConnected = False
 		if not data: 
 			serverConnected = False
-			#print("data: " + str(len(data)))
 		else:
 			theta = float(data)
 		xm1, ym1 = mousereader.getMousePosition()
-		#print ("Recieved data from client:" + str(theta))
-		#if not data: isConnected = False
-		#xm1 = 5.0
-		#ym1 = 0.0
 		deltaXm = xm1 - xm0
 		deltaYm = ym1 - ym0
 		if (deltaYm < 0):
@@ -77,15 +74,9 @@ while 1:
 		byf1 = bytes(syf1, 'utf-8')
 	
 		conn.send(bxf1)
-		#conn.recv(1024)
-		#print (conn.recv(1024))
 		conn.send(byf1)
 			
 		print ("alpha = " + str(alpha) + "   xf1 = " + str(xf1) + "   yf1 = " + str(yf1))
 	conn.close()
-	server_socket.close()
-	print ("Loop exited.")
-	#sys.exit()
-	#except KeyboardInterrupt:
-	#server_socket.close()	
+	print ("Loop exited.")	
 	
